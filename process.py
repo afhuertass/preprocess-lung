8892ed499df4389e36ff83dc12edd3947d91f0fe
Andres Huertas S <afhuertass@unal.edu.co>
2017-02-14 00:22:57 -0500

    2

--- process.py
+++ process.py
@@ -2,4 +2,13 @@
 
 """ firts shit on the cloud """
 
-print("yisus")
+from google.cloud import storage
+
+storage_client = storage.Client()
+
+bucket_name = "ml-new-bucket"
+
+
+bucket = storage_client.create_bucket(bucket_name)
+
+print('Bucket {} created.'.format(bucket.name))
